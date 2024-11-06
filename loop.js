19f6c1ae4f162ecc7b905381076811508a3a737d
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-11-06 10:18:03 -0500

    Add files via upload

--- loop.js
+++ loop.js
@@ -1,3 +1,34 @@
-/**
- * 
- */
+window.addEventListener("load", addListener);
+
+function addListener()
+{
+	document.getElementById("btnpress").addEventListener("click", Loop);
+}
+
+function Loop()
+{
+	var nument;
+	
+	nument = document.getElementById("txtentnum").value;
+	WhileLoop(nument);
+}
+
+function WhileLoop(entnum)
+{
+	let calcnum = 0, totalval = 0;
+	const elementname = document.getElementById("lblpress");
+	
+	while(calcnum <= entnum)
+	{
+		totalval = parseInt(calcnum) + parseInt(entnum);
+		if(calcnum == 0)
+		{
+			elementname.textContent = totalval;
+		}
+		else
+		{
+			elementname.textContent = elementname.textContent + ", " + totalval; 
+		}
+		calcnum = parseInt(calcnum) + 1;
+	}
+}
